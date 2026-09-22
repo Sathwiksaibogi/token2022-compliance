0cbd78798adcfc7b6ebd7ded6cfbce76fbd4f004
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-09-22 16:43:34 +0000

    first commit

---
+++ programs/token2022-compliance/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("Bzf35ZWCmtpzrYxQRd3y6x73NDHASarXkxK3RmFsmsW6");
+
+#[program]
+pub mod token2022_compliance {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
