c291b68461de554f352e92d6d1650cfd04e405c8
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-09-23 17:47:07 +0000

    feat(program):Add InitializeAuthorization context

---
+++ programs/token2022-compliance/src/instructions/initialize_authorization.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{token_2022::Token2022, token_interface::Mint};
+
+use crate::state::{Authorization, AuthorizationStatus, GlobalPolicy};
+
+#[derive(Accounts)]
+#[instruction(wallet:Pubkey)]
+
+pub struct InitializeAuthorization<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        owner=token_program.key(),
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds=[b"policy",mint.key().as_ref()],
+        bump=global_policy.bump,
+        has_one=admin,
+        has_one=mint,
+    )]
+    pub global_policy: Account<'info, GlobalPolicy>,
+
+    #[account(
+        init,
+        payer=admin,
+        space=8+Authorization::INIT_SPACE,
+        seeds=[b"authorization",mint.key().as_ref(),wallet.as_ref()],
+        bump,
+    )]
+    pub authorization: Account<'info, Authorization>,
+
+    pub token_program: Program<'info, Token2022>,
+
+    pub system_program: Program<'info, System>,
+}
--- programs/token2022-compliance/src/instructions/mod.rs
+++ programs/token2022-compliance/src/instructions/mod.rs
@@ -1,3 +1,5 @@
+pub mod initialize_authorization;
 pub mod initialize_policy;
 
+pub use initialize_authorization::*;
 pub use initialize_policy::*;
