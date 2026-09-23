7ca0a83cd9f578c509d7e2cfa0b80f0e94240072
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-09-23 13:12:43 +0000

    feat(program):Update Error

--- programs/token2022-compliance/src/error.rs
+++ programs/token2022-compliance/src/error.rs
@@ -1,9 +1,13 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Only the counter authority can update this counter")]
-    Unauthorized,
-    #[msg("Counter has reached the maximum value")]
-    CounterOverflow,
+pub enum ComplianceError {
+    #[msg("Maximum transfer amount must be greater than zero")]
+    InvalidMaxTransferAmount,
+
+    #[msg("Daily transfer limit must be at least the maximum transfer amount")]
+    InvalidDailyLimit,
+
+    #[msg("Expiration must be zero or a future Unix timestamp")]
+    InvalidExpiration,
 }
